52d7b8c90f454113299b3902faebd37fa9f3f2a3
LesyaLG <lg4lg666@gmail.com>
2022-12-09 13:27:34 +0200

    number1+number2 version1 :)

---
+++ resources/calc.js
@@ -0,0 +1,11 @@
+console.log('Calc loaded');
+
+document.querySelector('.calc-calculate').addEventListener('click',function()
+    { const number1 = +document.querySelector('[name="number1"]').value;
+const number2 = +document.querySelector('[name="number2"]').value;
+
+// + befor.  number1 and number2 converts a string to a number
+
+const sum = number1 + number2;
+document.querySelector('.calc-result').innerText=sum;
+    } )
