5a7320dbba11a9b2a3b0d69d43d9a992e8dcc14f
LesyaLG <lg4lg666@gmail.com>
2022-12-11 11:19:15 +0200

    Calc - Version 3. With all the checks and the scheme in the image folder

--- resources/calc.js
+++ resources/calc.js
@@ -1,29 +1,32 @@
 console.log('Calc loaded');
 
 function calculate()
-    { const number1 = +document.querySelector('[name="number1"]').value;
-      const number2 = +document.querySelector('[name="number2"]').value;
+    { const num1=document.querySelector('[name="number1"]').value;
+	  const num2=document.querySelector('[name="number2"]').value;
+	  let number1, number2;
+	  if (num1 !=="") number1 = +num1; else number1 = "";
+      if (num2 !=="") number2 = +num2; else number2 = "";
 	  const op = document.querySelector('[name="operation"]').value;
 
 	// + converts a string to a number
 
 	let result;
-	if (number1 !==0 && number2 !==0) 
-	 if (op==='plus'&& number2 !==0)   { result = number1 + number2; }
+	if (number1 !=="" && number2 !=="") 
+	 if (op==='plus')   { result = number1 + number2; }
 	  else 
 		if (op==='minus') { result = number1 - number2; }
 	      else 
 			if (op==='multiply') { result = number1 * number2; }
 		       else 
-				 if (op==='devide') { result = number1 / number2; }
-				  else {
+				if (op==='devide') 
+				   if (number2 !==0) { result = number1 / number2; }
+					 else {	result =""; console.error('Wrong operation');  alert('Division by 0 is impossible'); }
+				else {
 					   console.error('Wrong operation');
 					  alert('Wrong operation is selected');
 				  }
-	else 
-	{	result ="";
-		if(op==='devide') { console.error('Wrong operation');  alert('Division by 0 is impossible'); }
-	}
+	else  result =""; 
+	
 	document.querySelector('.calc-result').value=result;
     } 
 
