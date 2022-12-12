8a76cb4803c3d01a75e04a22f8d28d62c231ad87
Igor_Lazariev <dyema73@gmail.com>
2022-12-12 22:47:43 +0200

    calculator_adding_options

--- resources/calc.js
+++ resources/calc.js
@@ -2,7 +2,17 @@ console.log('loaded');
 document.querySelector('.calc').addEventListener('click', function() {
 	const sum1=+document.querySelector('input[name="num1"]').value;
 	const sum2=+document.querySelector('input[name="num2"]').value;
-	const sum3=sum1+sum2;
-	console.log(sum1, sum2, sum3);
-	document.querySelector('.calc-result').innerText=sum3;
+	const op=document.querySelector('.oper').value;
+	console.log(sum1, sum2, op);
+	let res;
+	if (op == 'plus') {
+	res=sum1+sum2;
+    } else	if (op == 'minus') {
+		res=sum1-sum2;
+	} else	if (op == 'mult') {
+		res=sum1*sum2;
+	} else	 {
+		res=sum1/sum2;
+	}
+	document.querySelector('.calc-result').innerText=res;
 });
