0a2aeeda987f6a8c3a1a8f93215495b7459a33d0
My Namevalpiatak <valpyatak@gmail.com>
2022-12-13 20:47:33 +0100

    embedded calc v 1.1

--- resources/clc.js
+++ resources/clc.js
@@ -1,6 +1,16 @@
 document.querySelector('.clc-clc') .addEventListener ('click', function () {
 	const n1 = 1*document.querySelector('[name="aygend"]' ).value;
 	const n2 = 1*document.querySelector('[name="addend"]' ).value;
-	const sum = n1+n2;
-	document.querySelector('.clc-res').value = sum;
+	const opr = document.querySelector('[name="opr"]' ).value;
+	let res;
+
+	if (opr=== 'plus') {res = n1+n2}
+	else if (opr=== 'minus') {res = n1-n2}
+	else if (opr=== 'multiply') {res = n1*n2}
+	else if (opr=== 'divide') {res = n1/n2}
+		else {console.error ('😳Wrong operation!');
+			          alert ('😳Wrong operation!');
+			 }
+
+	document.querySelector('.clc-res').value = res;
 });
