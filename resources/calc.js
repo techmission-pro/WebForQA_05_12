d886b5c8822e6537e2886c1f4e4936901906a517
romanyurch <romanyurchenko.30lk@gmail.com>
2022-12-19 19:13:13 +0200

    added file

--- resources/calc.js
+++ resources/calc.js
@@ -1,8 +1,9 @@
- document.querySelector('.calc-calculate').addEventListener('click', function () {
-     const num1 = +document.querySelector('[name="num1"]').value;
-	 const num2 = +document.querySelector('[name="num2"]').value;
+document.querySelector('.calc-calculate').addEventListener('click', function () {
+    const num1 = +document.querySelector('[name="num1"]').value;
+	const num2 = +document.querySelector('[name="num2"]').value;
 	 
-	 const sum = num1 + num2
+	const sum = num1 + num2
 	 
-	 document.querySelector('.calc-result').value = sum;
- });
+	document.querySelector('.calc-result').innerText = sum;
+});
+
