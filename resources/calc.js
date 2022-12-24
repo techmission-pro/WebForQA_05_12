d6759ef01451b94be2192f263fb316ef04905cd0
OlgaMarushchak <120114249+OlgaMarushchak@users.noreply.github.com>
2022-12-24 13:27:13 +0200

    Calculate_option

--- resources/calc.js
+++ resources/calc.js
@@ -2,8 +2,23 @@ document.querySelector('.calc-calculate').addEventListener('click', function (){
 const num1 = +document.querySelector('[name="num1"]').value;
 const num2 = +document.querySelector('[name="num2"]').value;
 
-const sum = num1 + num2;
+const option = document.querySelector('[name="option"]').value;
 
-document.querySelector('.calc-result').value = sum;
+let result;
+
+if (option === 'plus') {
+	result = num1 + num2;
+} else if (option === 'minus') {
+	result = num1 - num2;
+} else if (option === 'multiply') {
+	result = num1 * num2;
+} else if (option === 'divid') {
+	result = num1 / num2;
+} else {
+	colsole.error('Wrong op!');
+	alert('Wrong operation is selected');
+}
+
+document.querySelector('.calc-result').value = result;
 
 });
