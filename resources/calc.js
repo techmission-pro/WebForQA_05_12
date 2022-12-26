b82237b7748571c0faf560eb47f8c22464272095
Gennadii <gvizionok@gmail.com>
2022-12-26 20:00:23 +0200

    add calc
    Signed-off-by: Gennadii <gvizionok@gmail.com>

---
+++ resources/calc.js
@@ -0,0 +1,35 @@
+document.querySelector('.calculate').addEventListener('click', function() {
+	const num1 = +document.querySelector('[name="num1"]').value;
+	const num2 = +document.querySelector('[name="num2"]').value;
+	const option = document.querySelector('[name="option"]').value;
+	
+	let result;
+	
+	if(option === 'plus') (
+	result = num1 + num2;
+	) else if (option ==='minus') (
+	result = num1 - num2;
+	) else if (option ==='multiply') (
+	result = num1 * num2;
+	) else if (option ==='divide') (
+	result = num1 / num2;
+    ) else (
+       console.error('wrong operatition');
+       alert('wrong operation is selected');
+)
+
+	document.querySelector('.calculate-result').value = result;
+	
+}
+document.querySelector('.calculate').addEventListener('click', function() {
+	calculate ();
+)};
+document.querySelector('[name="option"]').addEventListener('change', function() {
+	calculate ();
+)};
+document.querySelector('[name="num1"]').addEventListener('input', function() {
+	calculate ();
+)};
+document.querySelector('[name="num2"]').addEventListener('input', function() {
+	calculate ();
+)};
