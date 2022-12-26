ca5566588cd13f25b0dfc8e7c038a408780e6c53
VedotovaK <asterkatya@gmail>
2022-12-26 14:47:53 +0200

    add different signs to calculator

--- resources/calculator.js
+++ resources/calculator.js
@@ -2,9 +2,25 @@
 document.querySelector('.calculator-calculate').addEventListener('click', function() {
     const num1 = +document.querySelector('[name = "num1"]').value;
     const num2 = +document.querySelector('[name = "num2"]').value;
+    const option = document.querySelector('[name="operation"]').value
 
-    const sum = num1 + num2;
+    let result;
+    if (option === 'plus') {
+        result =num1 + num2;
+    } else if (option === 'minus') {
+        result =num1 - num2;
+    }else if (option === 'multiply') {
+        result =num1 * num2;
+    }else if (option === 'divide') {
+    result =num1 / num2;
+    } else {
+        console.error('Wrong operation!');
+        alart ('Wrong operation is selected');
+    }
+
+
+
     
-    document.querySelector ('.calculator-result').value = sum;
-})
+    document.querySelector ('.calculator-result').value = result;
+});
     
